69ea67016509724a3428da385bf5b59cfb212f45
nikoagge <nikoagge@users.noreply.github.com>
2021-11-28 18:14:31 +0200

    Create getGenericRequest on NetworkClient service class.

---
+++ DuthDemonstrationProject/Enums/NetworkingError.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkingError.swift
+//  DuthDemonstrationProject
+//
+//  Created by Nikos Aggelidis on 28/11/21.
+//
+
+import Foundation
+
+enum NetworkingError: Error {
+    case invalidURL
+    case invalidData
+}
--- DuthDemonstrationProject/Networking/NetworkClient.swift
+++ DuthDemonstrationProject/Networking/NetworkClient.swift
@@ -12,12 +12,49 @@ class NetworkClient {
     
     let baseURL = "https://jsonplaceholder.typicode/com/"
     
-    func downloadPosts(with completion) {
+    func getUsers(with completion: @escaping ([User]?, Error?) -> ()) {
         let url = URL(string: baseURL + Path.posts.rawValue)
         guard let url = url else { return }
-
-        URLSession.shared.dataTask(with: url) { data, response, error in
-            <#code#>
+        DispatchQueue.main.async {
+            URLSession.shared.dataTask(with: url) { data, response, error in
+                if error != nil {
+                    completion(nil, error)
+                }
+                
+                if error == nil, let data = data {
+                    let users = try? JSONDecoder().decode([User].self, from: data)
+                    completion(users, nil)
+                }
+            }.resume()
         }
+    }
+}
+
+private extension NetworkClient {
+    func getGenericRequest<T: Codable> (for url: URL?, expectingTypeToDecode: T.Type, with completion: @escaping (Result<T, Error>) -> Void) {
+        guard let url = url else {
+            completion(.failure(NetworkingError.invalidURL))
+            
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url) { data, _, error in
+            guard let data = data else {
+                if let error = error {
+                    completion(.failure(error))
+                } else {
+                    completion(.failure(NetworkingError.invalidData))
+                }
+                
+                return
+            }
+            
+            do {
+                let result = try JSONDecoder().decode(expectingTypeToDecode, from: data)
+                completion(.success(result))
+            } catch {
+                completion(.failure(error))
+            }
+        }.resume()
     }
 }
